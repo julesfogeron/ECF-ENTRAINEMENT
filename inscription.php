a4001b7e06d11064ae132c24cb87a0339afb5715
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-12 14:12:36 +0100

    Maj Titre

--- inscription.php
+++ inscription.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>inscption</title>
+    <title>Inscription</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/inscription.css">
 </head>
