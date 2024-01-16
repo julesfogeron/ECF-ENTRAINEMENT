056e03faf9b829a0dd9bd544064e547c90e5d8dc
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-16 12:50:19 +0100

    modifier js

--- Activity 1/graph full.php
+++ Activity 1/graph full.php
@@ -12,6 +12,24 @@ session_start();
 ?>
 
 
+
+
+
+<?php include('headerConecter.php') ?>
+
+<div class="frame-1">
+    <div class="frame-13">
+        <label class="text" id="text" ></label>
+        <div class="frame-14">
+            <div class="graphique" onclick="changerImageGraphique()">Graphique</div>
+            <div class="line-1"></div>
+            <div class="trading-view" onclick="changerImageTradingView()">Trading view</div>
+        </div>
+    </div>
+    <div class="image-3" >
+        <img class="image-6" src="image/image-Bitcoin.png" id="image" alt="Graphique"/>
+    </div>
+</div>
 <script>
     const objet = location.search.substring(1).split('=')[1];
     console.log(objet)
@@ -33,24 +51,6 @@ session_start();
         img.src = "image/image-"+ objet +"-Trading-View.png";
     }
 </script>
-
-
-<?php include('headerConecter.php') ?>
-
-<div class="frame-1">
-    <div class="frame-13">
-        <label class="text" id="text" ></label>
-        <div class="frame-14">
-            <div class="graphique" onclick="changerImageGraphique()">Graphique</div>
-            <div class="line-1"></div>
-            <div class="trading-view" onclick="changerImageTradingView()">Trading view</div>
-        </div>
-    </div>
-    <div class="image-3" >
-        <img class="image-6" src="image/image-Bitcoin.png" id="image" alt="Graphique"/>
-    </div>
-</div>
-
 
 <?php include('footer.html') ?>
 </body>
